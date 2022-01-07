4681c10153bf466cc1ef0b03998dceb208cdc9ce
byusa <byusa.serge@gmail.com>
2022-01-06 16:07:51 -0800

    accepted current changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import DragDrop from "./components/DragDrop";
+import "./App.css";
+const finalSpaceCharacters = [
+  {
+    id: "gary",
+    name: "Gary Goodspeed",
+    thumb: "/images/gary.png",
+  },
+  {
+    id: "cato",
+    name: "Little Cato",
+    thumb: "/images/cato.png",
+  },
+  {
+    id: "kvn",
+    name: "KVN",
+    thumb: "/images/kvn.png",
+  },
+  {
+    id: "mooncake",
+    name: "Mooncake",
+    thumb: "/images/mooncake.png",
+  },
+  {
+    id: "quinn",
+    name: "Quinn Ergon",
+    thumb: "/images/quinn.png",
+  },
+];
 function App() {
+  const [characters, updateCharacters] = useState(finalSpaceCharacters);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Final Space Characters</h1>
+        <DragDrop data={characters} />
       </header>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/DragDrop.js
@@ -0,0 +1,98 @@
+// import React, { useState } from "react";
+// import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+// import "../App.css";
+
+// const DragDrop = (props) => {
+//   const [characters, updateCharacters] = useState(props.data);
+
+//   function handleOnDragEnd(result) {
+//     if (!result.destination) return;
+
+//     const items = Array.from(characters);
+//     const [reorderedItem] = items.splice(result.source.index, 1);
+//     items.splice(result.destination.index, 0, reorderedItem);
+
+//     updateCharacters(items);
+//   }
+//   return (
+//     <DragDropContext onDragEnd={handleOnDragEnd}>
+//       <Droppable droppableId="characters">
+//         {(provided) => (
+//           <ul
+//             className="characters"
+//             {...provided.droppableProps}
+//             ref={provided.innerRef}
+//           >
+//             {characters.map(({ id, name, thumb }, index) => {
+//               return (
+//                 <Draggable key={id} draggableId={id} index={index}>
+//                   {(provided) => (
+//                     <li
+//                       ref={provided.innerRef}
+//                       {...provided.draggableProps}
+//                       {...provided.dragHandleProps}
+//                     >
+//                       <div className="characters-thumb">
+//                         <img src={thumb} alt={`${name} Thumb`} />
+//                       </div>
+//                       <p>{name}</p>
+//                     </li>
+//                   )}
+//                 </Draggable>
+//               );
+//             })}
+//             {provided.placeholder}
+//           </ul>
+//         )}
+//       </Droppable>
+//     </DragDropContext>
+//   );
+// };
+
+// export default DragDrop;
+
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import "../App.css";
+
+const DragDrop = (props) => {
+  const [elements, updateElements] = useState(props.data);
+
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+
+    const items = Array.from(elements);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    updateElements(items);
+  }
+  return (
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="elements">
+        {(provided) => (
+          <ul className="" {...provided.droppableProps} ref={provided.innerRef}>
+            {elements.map(({ id, name }, index) => {
+              return (
+                <Draggable key={id} draggableId={id} index={index}>
+                  {(provided) => (
+                    <li
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <p>{name}</p>
+                    </li>
+                  )}
+                </Draggable>
+              );
+            })}
+            {provided.placeholder}
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+};
+
+export default DragDrop;
